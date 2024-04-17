a23160be8d680383ecdcb5ff3d60f20881d46ed1
porcuquine <porcuquine@users.noreply.github.com>
2024-04-16 17:53:12 -0700

    Refactor terms.

--- examples/ski/ski/src/lib.rs
+++ examples/ski/ski/src/lib.rs
@@ -2,8 +2,6 @@ pub mod air;
 pub mod chips;
 pub mod cons;
 pub mod cpu;
-pub mod interned;
 pub mod terms;
 
-pub use crate::interned::{ITerm, Mem};
-pub use crate::terms::{ParseTermError, SKI};
+pub use crate::terms::{Mem, ParseTermError, Term, SKI};
