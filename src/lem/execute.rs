afb0f956769e1cce7f1c8dd270a0e30315d2d462
Arthur Paulino <arthurleonardo.ap@gmail.com>
2024-04-25 09:42:53 -0300

    chore: minor ergonomic changes

--- src/lem/execute.rs
+++ src/lem/execute.rs
@@ -136,7 +136,7 @@ impl<F: Field + Ord> Op<F> {
 mod tests {
     use crate::{
         func,
-        lem::{execute::ExecuteRecord, toplevel::Toplevel, Name},
+        lem::{execute::ExecuteRecord, toplevel::Toplevel},
     };
 
     use p3_baby_bear::BabyBear as F;
@@ -144,7 +144,7 @@ mod tests {
 
     #[test]
     fn lem_execute_test() {
-        let factorial = func!(
+        let factorial_e = func!(
         fn factorial(n): 1 {
             let one = num(1);
             match n {
@@ -157,7 +157,8 @@ mod tests {
             let res = mul(n, m);
             return res
         });
-        let even = func!(
+
+        let even_e = func!(
         fn even(n): 1 {
             let one = num(1);
             match n {
@@ -169,7 +170,8 @@ mod tests {
             let res = call(odd, pred);
             return res
         });
-        let odd = func!(
+
+        let odd_e = func!(
         fn odd(n): 1 {
             let one = num(1);
             match n {
@@ -182,22 +184,23 @@ mod tests {
             let res = call(even, pred);
             return res
         });
-        let toplevel = Toplevel::new(&[even, factorial, odd]);
 
-        let factorial = toplevel.map().get(&Name("factorial")).unwrap();
+        let toplevel = Toplevel::new(&[even_e, factorial_e, odd_e]);
+
+        let factorial = toplevel.get_func("factorial").unwrap();
         let mut args = vec![F::from_canonical_u32(5)];
         let record = &mut ExecuteRecord::new(&toplevel);
         let out = factorial.execute(&mut args, &toplevel, record);
         assert_eq!(out.len(), 1);
         assert_eq!(out[0], F::from_canonical_u32(120));
 
-        let even = toplevel.map().get(&Name("even")).unwrap();
+        let even = toplevel.get_func("even").unwrap();
         let mut args = vec![F::from_canonical_u32(7)];
         let out = even.execute(&mut args, &toplevel, record);
         assert_eq!(out.len(), 1);
         assert_eq!(out[0], F::from_canonical_u32(0));
 
-        let odd = toplevel.map().get(&Name("odd")).unwrap();
+        let odd = toplevel.get_func("odd").unwrap();
         let mut args = vec![F::from_canonical_u32(7)];
         let out = odd.execute(&mut args, &toplevel, record);
         assert_eq!(out.len(), 1);
