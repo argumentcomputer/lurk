708dfad1fa3a6f3376312e876b30f5861298c7ee
Arthur Paulino <arthurleonardo.ap@gmail.com>
2024-05-08 21:49:54 -0300

    documentation for lair/map

--- src/lair/map.rs
+++ src/lair/map.rs
@@ -1,27 +1,33 @@
+//! This module defines a map optimized for the purpose of declaring Lair programs
+
 use std::slice::Iter;
 
+/// A non-extensible map intended for a small number of entries. Data access is:
+/// * O(1) by index
+/// * Linear up to `LINEAR_SEARCH_MAX`
+/// * O(log n) otherwise
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Map<K, V>(Vec<(K, V)>);
+pub struct Map<K, V>(Box<[(K, V)]>);
 
-pub type Set<K> = Map<K, ()>;
-
-const LINEAR_SEARCH_MAX: usize = 16;
+/// Maximum number of entries for which linear search is performed
+const LINEAR_SEARCH_MAX: usize = 15;
 
 impl<K: Ord, V> Map<K, V> {
+    /// Creates a `Map` from a vector by sorting it by key beforehand
+    ///
+    /// # Panics
+    /// Panics if a duplicated key is found
     pub fn from_vec(mut pairs: Vec<(K, V)>) -> Self {
         pairs.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
         pairs
             .windows(2)
             .for_each(|p| assert!(p[0].0 != p[1].0, "Duplicate pattern found"));
-        Self(pairs)
+        Self(pairs.into())
     }
 
-    pub(crate) fn from_vec_unsafe(pairs: Vec<(K, V)>) -> Self {
-        Self(pairs)
-    }
-
+    /// Retrieves a value by its key
     pub fn get(&self, f: &K) -> Option<&V> {
-        if self.0.len() < LINEAR_SEARCH_MAX {
+        if self.0.len() <= LINEAR_SEARCH_MAX {
             self.0.iter().find(|(g, _)| f == g).map(|(_, v)| v)
         } else {
             let t = self.0.binary_search_by(|(g, _)| g.cmp(f)).ok();
@@ -29,13 +35,10 @@ impl<K: Ord, V> Map<K, V> {
         }
     }
 
+    /// Retrieves the index by key
     pub fn get_index_of(&self, f: &K) -> Option<usize> {
-        if self.0.len() < LINEAR_SEARCH_MAX {
-            self.0
-                .iter()
-                .enumerate()
-                .find(|(_, (g, _))| f == g)
-                .map(|(i, _)| i)
+        if self.0.len() <= LINEAR_SEARCH_MAX {
+            self.0.iter().position(|(g, _)| f == g)
         } else {
             self.0.binary_search_by(|(g, _)| g.cmp(f)).ok()
         }
@@ -43,21 +46,42 @@ impl<K: Ord, V> Map<K, V> {
 }
 
 impl<K, V> Map<K, V> {
+    /// Creates a `Map` from a vector without sorting
+    ///
+    /// # Warning
+    /// It will create an inconsistent map if the pairs aren't already sorted
     #[inline]
-    pub fn to_vec(self) -> Vec<(K, V)> {
+    pub(crate) fn from_vec_unsafe(pairs: Vec<(K, V)>) -> Self {
+        Self(pairs.into())
+    }
+}
+
+impl<K, V> Map<K, V> {
+    /// Returns the slice of pairs
+    #[inline]
+    pub fn get_pairs(&self) -> &[(K, V)] {
+        &self.0
+    }
+
+    /// Returns the boxed slice of pairs
+    #[inline]
+    pub fn into_pairs(self) -> Box<[(K, V)]> {
         self.0
     }
 
+    /// Retrieves the key/value pair by intex
     #[inline]
     pub fn get_index(&self, i: usize) -> Option<&(K, V)> {
         self.0.get(i)
     }
 
+    /// Returns an iterator over the key/value pairs
     #[inline]
     pub fn iter(&self) -> Iter<'_, (K, V)> {
         self.0.iter()
     }
 
+    /// The number of entries
     #[inline]
     pub fn size(&self) -> usize {
         self.0.len()
@@ -86,7 +110,7 @@ mod tests {
             });
         };
 
-        test_with_len(LINEAR_SEARCH_MAX - 1); // testing linear search
-        test_with_len(LINEAR_SEARCH_MAX); // testing binary search
+        test_with_len(LINEAR_SEARCH_MAX); // testing linear search
+        test_with_len(LINEAR_SEARCH_MAX + 1); // testing binary search
     }
 }
