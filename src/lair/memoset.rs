792a6c212a660bcbcd4e76dbc0f8f7851468897a
Adrian Hamelink <adrian.hamelink@gmail.com>
2024-06-06 10:29:22 +0200

    Fix memoset constraint

--- src/lair/memoset.rs
+++ src/lair/memoset.rs
@@ -29,24 +29,25 @@ where
         builder.assert_bool(is_provide);
 
         let is_real = is_require + is_provide;
-        {
-            let is_real_next = is_require_next + is_provide_next;
-            builder.assert_bool(is_real.clone());
-            // is_real starts with one
-            builder.when_first_row().assert_one(is_real.clone());
-            // if next is real, local is real
-            builder
-                .when_transition()
-                .when(is_real_next.clone())
-                .assert_one(is_real.clone());
 
-            let is_last_real = builder.is_transition() * (AB::Expr::one() - is_real_next)
-                + builder.is_last_row() * is_real;
-            // when the next row is not real, ensure the last query was provided
-            builder.when(is_last_real).assert_one(is_provide)
-        }
+        let is_real_next = is_require_next + is_provide_next;
+        builder.assert_bool(is_real.clone());
+        // is_real starts with one
+        builder.when_first_row().assert_one(is_real.clone());
+        // if next is real, local is real
+        builder
+            .when_transition()
+            .when(is_real_next.clone())
+            .assert_one(is_real.clone());
 
-        // A require query must repeat to the next row
+        // The current row is real, and either it is the last row, or the next row is not real.
+        let is_last_real = is_real
+            * (builder.is_last_row() + builder.is_transition() * (AB::Expr::one() - is_real_next));
+
+        // when the next row is not real, ensure the last query was provided
+        builder.when(is_last_real).assert_one(is_provide);
+
+        // A require query must repeat to the next row, until it is provided.
         for (&val_local, &val_next) in zip(query_local, query_next) {
             builder.when(is_require).assert_eq(val_local, val_next);
         }
