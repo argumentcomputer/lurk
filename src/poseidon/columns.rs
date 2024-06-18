bd5922a74768a8d66afc6634f4a73c4e812a5149
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2024-06-18 15:42:50 -0400

    Single row Poseidon (#88)
    AIR constraints and trace generation for a single-row Poseidon 2 implementation.
    
    ---------
    
    Co-authored-by: Adrian Hamelink <adrian.hamelink@gmail.com>

--- src/poseidon/columns.rs
+++ src/poseidon/columns.rs
@@ -1,68 +1,68 @@
 //! This module defines the columns for the Poseidon 2 chip
 
+use core::array;
 use std::iter::zip;
 use std::mem::size_of;
 
-use crate::poseidon::util::{matmul_exterior, matmul_internal};
+use super::config::PoseidonConfig;
+
 use hybrid_array::Array;
 use p3_field::AbstractField;
-
-use super::config::PoseidonConfig;
+use p3_symmetric::Permutation;
 
 /// The column layout for the chip.
 #[derive(Clone, Debug)]
 #[repr(C)]
-pub struct Poseidon2Cols<T, C>
-where
-    C: PoseidonConfig,
-{
-    pub(crate) input: Array<T, C::WIDTH>,
+pub struct Poseidon2Cols<T, C: PoseidonConfig<WIDTH>, const WIDTH: usize> {
+    pub(crate) input: [T; WIDTH],
     pub(crate) is_init: T,
     pub(crate) rounds: Array<T, C::R>,
-    pub(crate) add_rc: Array<T, C::WIDTH>,
-    pub(crate) sbox_deg_3: Array<T, C::WIDTH>,
-    pub(crate) sbox_deg_7: Array<T, C::WIDTH>,
-    pub(crate) output: Array<T, C::WIDTH>,
+    pub(crate) add_rc: [T; WIDTH],
+    pub(crate) sbox_deg_3: [T; WIDTH],
+    pub(crate) sbox_deg_7: [T; WIDTH],
+    pub(crate) output: [T; WIDTH],
 }
 
-impl<T, C: PoseidonConfig> Poseidon2Cols<T, C> {
+impl<T, C: PoseidonConfig<WIDTH>, const WIDTH: usize> Poseidon2Cols<T, C, WIDTH> {
     #[inline]
     pub fn from_slice(slice: &[T]) -> &Self {
-        let num_cols = size_of::<Poseidon2Cols<u8, C>>();
+        let num_cols = size_of::<Poseidon2Cols<u8, C, WIDTH>>();
         assert_eq!(slice.len(), num_cols);
-        let (_, shorts, _) = unsafe { slice.align_to::<Poseidon2Cols<T, C>>() };
+        let (_, shorts, _) = unsafe { slice.align_to::<Poseidon2Cols<T, C, WIDTH>>() };
         &shorts[0]
     }
 }
 
-impl<C: PoseidonConfig> Poseidon2Cols<C::F, C> {
-    pub fn set_initial_round(&mut self, input: Array<C::F, C::WIDTH>) -> Array<C::F, C::WIDTH> {
-        self.input = input.clone();
+impl<C: PoseidonConfig<WIDTH>, const WIDTH: usize> Poseidon2Cols<C::F, C, WIDTH> {
+    pub fn set_initial_round(&mut self, input: [C::F; WIDTH]) -> [C::F; WIDTH] {
+        self.input = input;
         self.is_init = C::F::one();
-        self.add_rc = input.clone();
+        self.add_rc = input;
         self.evaluate_sbox();
         self.output = input;
 
-        matmul_exterior(&mut self.output);
-        self.output.clone()
+        C::external_linear_layer().permute_mut(&mut self.output);
+        self.output
     }
 
     pub fn set_round(
         &mut self,
-        input: Array<C::F, C::WIDTH>,
+        input: [C::F; WIDTH],
         round: usize,
         constants: &[C::F],
-    ) -> Array<C::F, C::WIDTH> {
+    ) -> [C::F; WIDTH] {
         self.input = input;
 
         // Set the boolean flags
-        let is_external_first_half = round < C::R_F / 2;
-        let is_external_second_half = round >= C::R_F / 2 + C::R_P;
+        let rounds_half = C::r_f() / 2;
+        let partial_rounds = C::r_p();
+        let is_external_first_half = round < rounds_half;
+        let is_external_second_half = round >= rounds_half + partial_rounds;
         let is_external = is_external_first_half || is_external_second_half;
 
         self.rounds[round] = C::F::one();
 
-        self.add_rc = self.input.clone();
+        self.add_rc = self.input;
         // Apply the round constants
         // Internal round: constants.len() = 1
         // External round: constants.len() = WIDTH
@@ -73,7 +73,7 @@ impl<C: PoseidonConfig> Poseidon2Cols<C::F, C> {
         // Evaluate sbox over add_rc
         self.evaluate_sbox();
 
-        let mut linear_input: Array<C::F, C::WIDTH> = Array::from_fn(|i| {
+        let mut linear_input: [C::F; WIDTH] = array::from_fn(|i| {
             if i == 0 || is_external {
                 self.sbox_deg_7[i]
             } else {
@@ -83,19 +83,18 @@ impl<C: PoseidonConfig> Poseidon2Cols<C::F, C> {
 
         // Apply the linear layer
         if is_external {
-            matmul_exterior(&mut linear_input)
+            C::external_linear_layer().permute_mut(&mut linear_input)
         } else {
-            let matmul_constants = C::matrix_diag().iter().copied();
-            matmul_internal(&mut linear_input, matmul_constants);
+            C::internal_linear_layer().permute_mut(&mut linear_input);
         }
 
         self.output = linear_input;
 
-        self.output.clone()
+        self.output
     }
 
     fn evaluate_sbox(&mut self) {
-        self.sbox_deg_3 = Array::from_fn(|i| self.add_rc[i].cube());
-        self.sbox_deg_7 = Array::from_fn(|i| self.sbox_deg_3[i].square() * self.add_rc[i]);
+        self.sbox_deg_3 = array::from_fn(|i| self.add_rc[i].cube());
+        self.sbox_deg_7 = array::from_fn(|i| self.sbox_deg_3[i].square() * self.add_rc[i]);
     }
 }
--- src/poseidon/trace.rs
+++ src/poseidon/trace.rs
@@ -1,21 +1,19 @@
 //! This module defines the trace generation for the Poseidon2 AIR Chip
-use hybrid_array::Array;
-use itertools::Itertools;
-use p3_air::BaseAir;
-use p3_field::AbstractField;
-use p3_matrix::dense::RowMajorMatrix;
 use std::iter::zip;
 
 use super::{columns::Poseidon2Cols, config::PoseidonConfig, Poseidon2Chip};
 
-impl<C> Poseidon2Chip<C>
-where
-    C: PoseidonConfig,
-{
-    pub fn generate_trace(&self, inputs: Vec<Array<C::F, C::WIDTH>>) -> RowMajorMatrix<C::F> {
-        // let width = C::WIDTH;
-        let rounds = C::R_F + C::R_P;
-        let num_cols = <Poseidon2Chip<C> as BaseAir<C::F>>::width(self);
+use itertools::Itertools;
+use p3_air::BaseAir;
+use p3_field::AbstractField;
+use p3_matrix::dense::RowMajorMatrix;
+
+impl<C: PoseidonConfig<WIDTH>, const WIDTH: usize> Poseidon2Chip<C, WIDTH> {
+    pub fn generate_trace(&self, inputs: Vec<[C::F; WIDTH]>) -> RowMajorMatrix<C::F> {
+        let full_rounds = C::r_f();
+        let part_rounds = C::r_p();
+        let rounds = full_rounds + part_rounds;
+        let num_cols = <Poseidon2Chip<C, WIDTH> as BaseAir<C::F>>::width(self);
 
         let full_num_rows = inputs.len() * (rounds + 1);
         let full_trace_len_padded = full_num_rows.next_power_of_two() * num_cols;
@@ -23,7 +21,7 @@ where
         let mut trace = RowMajorMatrix::new(vec![C::F::zero(); full_trace_len_padded], num_cols);
 
         let (prefix, rows, suffix) =
-            unsafe { trace.values.align_to_mut::<Poseidon2Cols<C::F, C>>() };
+            unsafe { trace.values.align_to_mut::<Poseidon2Cols<C::F, C, WIDTH>>() };
         assert!(prefix.is_empty(), "Alignment should match");
         assert!(suffix.is_empty(), "Alignment should match");
         assert_eq!(rows.len(), full_num_rows.next_power_of_two());
