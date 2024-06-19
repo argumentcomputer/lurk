4e7f2a61c4ebadef59dce9c12b97c2fb5551f1f4
Arthur Paulino <arthurleonardo.ap@gmail.com>
2024-06-19 10:51:11 -0300

    create LairChip enum and implement necessary traits for it

--- src/lair/chip.rs
+++ src/lair/chip.rs
@@ -1,8 +1,15 @@
-use p3_air::BaseAir;
+use p3_air::{Air, AirBuilder, BaseAir};
+use p3_field::{Field, PrimeField};
+use p3_matrix::dense::RowMajorMatrix;
+use sphinx_core::air::{EventLens, MachineAir, WithEvents};
+
+use crate::air::builder::LookupBuilder;
 
 use super::{
     bytecode::{Block, Ctrl, Func, Op},
+    execute::QueryRecord,
     hasher::Hasher,
+    memory::MemChip,
     toplevel::Toplevel,
     List,
 };
@@ -267,6 +274,105 @@ impl<F> Op<F> {
                 degrees.extend(vec![1; witness_size]);
             }
             Op::Debug(..) => (),
+        }
+    }
+}
+
+impl<'a, 'b, F: Field, H: Hasher<F>> WithEvents<'a> for FuncChip<'b, F, H> {
+    type Events = &'a QueryRecord<F>;
+}
+
+impl<'a, F: Field, H: Hasher<F>> EventLens<FuncChip<'a, F, H>> for QueryRecord<F> {
+    fn events(&self) -> <FuncChip<'a, F, H> as WithEvents<'_>>::Events {
+        self
+    }
+}
+
+impl<'a, F: PrimeField, H: Hasher<F>> MachineAir<F> for FuncChip<'a, F, H> {
+    type Record = QueryRecord<F>;
+    type Program = QueryRecord<F>;
+
+    fn name(&self) -> String {
+        self.func.name.to_string()
+    }
+
+    fn generate_trace<EL: EventLens<Self>>(
+        &self,
+        input: &EL,
+        _: &mut Self::Record,
+    ) -> RowMajorMatrix<F> {
+        self.generate_trace_parallel(input.events())
+    }
+
+    fn included(&self, _: &Self::Record) -> bool {
+        true
+    }
+
+    fn generate_dependencies<EL: EventLens<Self>>(&self, _: &EL, _: &mut Self::Record) {}
+}
+
+pub enum LairChip<'a, F, H: Hasher<F>> {
+    Func(FuncChip<'a, F, H>),
+    Mem(MemChip<F>),
+}
+
+impl<'a, 'b, F: Field, H: Hasher<F>> WithEvents<'a> for LairChip<'b, F, H> {
+    type Events = &'a QueryRecord<F>;
+}
+
+impl<'a, F: Field, H: Hasher<F>> EventLens<LairChip<'a, F, H>> for QueryRecord<F> {
+    fn events(&self) -> <LairChip<'a, F, H> as WithEvents<'_>>::Events {
+        self
+    }
+}
+
+impl<'a, F: Sync, H: Hasher<F>> BaseAir<F> for LairChip<'a, F, H> {
+    fn width(&self) -> usize {
+        match self {
+            Self::Func(func_chip) => func_chip.width(),
+            Self::Mem(mem_chip) => mem_chip.width(),
+        }
+    }
+}
+
+impl<'a, F: PrimeField, H: Hasher<F>> MachineAir<F> for LairChip<'a, F, H> {
+    type Record = QueryRecord<F>;
+    type Program = QueryRecord<F>;
+
+    fn name(&self) -> String {
+        match self {
+            Self::Func(func_chip) => format!("Func[{}]", func_chip.name()),
+            Self::Mem(mem_chip) => format!("Mem[{}]", mem_chip.name()),
+        }
+    }
+
+    fn generate_trace<EL: EventLens<Self>>(
+        &self,
+        input: &EL,
+        output: &mut Self::Record,
+    ) -> RowMajorMatrix<F> {
+        match self {
+            Self::Func(func_chip) => func_chip.generate_trace_parallel(input.events()),
+            Self::Mem(mem_chip) => mem_chip.generate_trace(input.events(), output),
+        }
+    }
+
+    fn included(&self, _: &Self::Record) -> bool {
+        true
+    }
+
+    fn generate_dependencies<EL: EventLens<Self>>(&self, _: &EL, _: &mut Self::Record) {}
+}
+
+impl<'a, AB, H: Hasher<AB::F>> Air<AB> for LairChip<'a, AB::F, H>
+where
+    AB: AirBuilder + LookupBuilder,
+    <AB as AirBuilder>::Var: std::fmt::Debug,
+{
+    fn eval(&self, builder: &mut AB) {
+        match self {
+            Self::Func(func_chip) => func_chip.eval(builder),
+            Self::Mem(mem_chip) => mem_chip.eval(builder),
         }
     }
 }
