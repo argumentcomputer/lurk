fbe2683fcd7ac8c138a480f0b946d4396fb6de84
wwared <wwared@users.noreply.github.com>
2024-07-17 10:22:18 -0300

    chore: Remove cargo template code (#151)
    Co-authored-by: wwared <541936+wwared@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,3 @@ pub mod gadgets;
 pub mod lurk;
 pub mod poseidon;
 pub mod store_core;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
