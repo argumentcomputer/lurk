92c1432184877cc2c12e698af043e68338300713
Arthur Paulino <arthurleonardo.ap@gmail.com>
2024-08-16 11:44:41 -0300

    feat: show commit hash when the app starts (#220)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
+    println!(
+        "commit: {} {}",
+        env!("VERGEN_GIT_COMMIT_DATE"),
+        env!("VERGEN_GIT_SHA")
+    );
     loam::lurk::cli::run()
 }
